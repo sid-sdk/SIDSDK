b42ab705e9205f11a76e2eaaf121bab253cebca3
Олюшкин Максим Константинович <MKoOlyushkin@sberbank.ru>
2025-03-21 18:24:54 +0300

    0.0.1-cs

--- Package.swift
+++ Package.swift
@@ -9,66 +9,20 @@ let package = Package(
 	],
 	products: [
 		.library(
-			name: "SIDSDKDynamic",
-			targets: ["SIDSDKDynamicWrapper"]
-		),
-		.library(
-			name: "SIDSDKStatic",
-			targets: ["SIDSDKStaticWrapper"]
-		),
-		.library(
 			name: "SIDSDKDynamic-CS",
 			targets: ["SIDSDKDynamic-CSBinary"]
 		),
 		.library(
 			name: "SIDSDKStatic-CS",
 			targets: ["SIDSDKStaticWrapper-CS"]
-		),
-		.library(
-			name: "ClickstreamAnalytics",
-			targets: ["ClickstreamAnalytics"]
 		)
 	],
 	targets: [
-		// Бинарная цель для динамической библиотеки
-		.target(
-			name: "SIDSDKDynamicWrapper",
-			dependencies: [
-				.target(name: "SIDSDKDynamicBinary"),
-				.target(name: "ClickstreamAnalytics")
-			]
-		),
-		.binaryTarget(
-			name: "SIDSDKDynamicBinary",
-			path: "XCFrameworks/SIDSDKDynamic.zip"
-		),
-
-
-		// Обёртка над статической библиотекой, куда «прокидываем» bundle
-		.target(
-			name: "SIDSDKStaticWrapper",
-			dependencies: [
-				.target(name: "SIDSDKStaticBinary"),
-				.target(name: "ClickstreamAnalytics")
-			],
-			exclude: ["SIDSDKResourcesBundle.bundle/Info.plist"],
-			resources: [
-				.process("SIDSDKResourcesBundle.bundle")
-			]
-		),
-		// Бинарная цель для статической библиотеки
-		.binaryTarget(
-			name: "SIDSDKStaticBinary",
-			path: "./XCFrameworks/SIDSDKStatic.zip"
-		),
-
-
 		// Бинарная цель для динамической библиотеки без Кликстрима
 		.binaryTarget(
 			name: "SIDSDKDynamic-CSBinary",
 			path: "XCFrameworks/SIDSDKDynamic-CS.zip"
 		),
-
 
 		// Обёртка над статической библиотекой, куда «прокидываем» bundle. Без Кликстрима
 		.target(
@@ -85,13 +39,6 @@ let package = Package(
 		.binaryTarget(
 			name: "SIDSDKStaticBinary-CS",
 			path: "./XCFrameworks/SIDSDKStatic-CS.zip"
-		),
-
-
-		// Библиотека для отправки аналитики
-		.binaryTarget(
-			name: "ClickstreamAnalytics",
-			path: "XCFrameworks/ClickstreamAnalytics.zip"
-		),
+		)
 	]
 )
