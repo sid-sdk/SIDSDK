c933afcc3933eec50b568911a45e8e6914d0c4ec
Valieva Anna <aezabluda@sberbank.ru>
2025-04-03 12:23:31 +0300

    Релиз 1.5.0

--- Package.swift
+++ Package.swift
@@ -15,14 +15,6 @@ let package = Package(
 		.library(
 			name: "SIDSDKStatic",
 			targets: ["SIDSDKStaticWrapper"]
-		),
-		.library(
-			name: "SIDSDKDynamic-CS",
-			targets: ["SIDSDKDynamic-CSBinary"]
-		),
-		.library(
-			name: "SIDSDKStatic-CS",
-			targets: ["SIDSDKStaticWrapper-CS"]
 		)
 	],
 	dependencies: [
@@ -45,7 +37,6 @@ let package = Package(
 			path: "XCFrameworks/SIDSDKDynamic.zip"
 		),
 
-
 		// Обёртка над статической библиотекой, куда «прокидываем» bundle
 		.target(
 			name: "SIDSDKStaticWrapper",
@@ -62,31 +53,6 @@ let package = Package(
 		.binaryTarget(
 			name: "SIDSDKStaticBinary",
 			path: "./XCFrameworks/SIDSDKStatic.zip"
-		),
-
-
-		// Бинарная цель для динамической библиотеки без Кликстрима
-		.binaryTarget(
-			name: "SIDSDKDynamic-CSBinary",
-			path: "XCFrameworks/SIDSDKDynamic-CS.zip"
-		),
-
-
-		// Обёртка над статической библиотекой, куда «прокидываем» bundle. Без Кликстрима
-		.target(
-			name: "SIDSDKStaticWrapper-CS",
-			dependencies: [
-				.target(name: "SIDSDKStaticBinary-CS")
-			],
-			exclude: ["SIDSDKResourcesBundle.bundle/Info.plist"],
-			resources: [
-				.process("SIDSDKResourcesBundle.bundle")
-			]
-		),
-		// Бинарная цель для статической библиотеки без Кликстрима
-		.binaryTarget(
-			name: "SIDSDKStaticBinary-CS",
-			path: "./XCFrameworks/SIDSDKStatic-CS.zip"
 		)
 	]
 )
