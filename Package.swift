622cdf8b02e0b781b5e6412abe61e3355db84b83
Олюшкин Максим Константинович <MKoOlyushkin@sberbank.ru>
2025-11-12 17:12:14 +0300

    Релизная версия SIDSDK 1.8.0-cs

--- Package.swift
+++ Package.swift
@@ -9,14 +9,6 @@ let package = Package(
 	],
 	products: [
 		.library(
-			name: "SIDSDKDynamic",
-			targets: ["SIDSDKDynamicWrapper"]
-		),
-		.library(
-			name: "SIDSDKStatic",
-			targets: ["SIDSDKStaticWrapper"]
-		),
-		.library(
 			name: "SIDSDKDynamic-CS",
 			targets: ["SIDSDKDynamic-CSBinary"]
 		),
@@ -25,57 +17,18 @@ let package = Package(
 			targets: ["SIDSDKStaticWrapper_CS"]
 		)
 	],
-	dependencies: [
-		.package(
-			url: "https://gitverse.ru/clickstream/iOS-SDK-Package.git",
-			exact: "1.6.0"
-		)
-	],
 	targets: [
-		// Бинарная цель для динамической библиотеки
-		.target(
-			name: "SIDSDKDynamicWrapper",
-			dependencies: [
-				.target(name: "SIDSDKDynamicBinary"),
-				.product(name: "Clickstream", package: "iOS-SDK-Package")
-			]
-		),
-		.binaryTarget(
-			name: "SIDSDKDynamicBinary",
-			path: "XCFrameworks/SIDSDKDynamic.zip"
-		),
-
-
-		// Обёртка над статической библиотекой, куда «прокидываем» bundle
-		.target(
-			name: "SIDSDKStaticWrapper",
-			dependencies: [
-				"SIDSDKStaticBinary",
-				.product(name: "Clickstream", package: "iOS-SDK-Package")
-			],
-			exclude: ["SIDSDKResourcesBundle.bundle/Info.plist"], // исключаем Info.plist из bundle
-			resources: [
-				.process("SIDSDKResourcesBundle.bundle")
-			]
-		),		// Бинарная цель для статической библиотеки
-		.binaryTarget(
-			name: "SIDSDKStaticBinary",
-			path: "./XCFrameworks/SIDSDKStatic.zip"
-		),
-
-
 		// Бинарная цель для динамической библиотеки без Кликстрима
 		.binaryTarget(
 			name: "SIDSDKDynamic-CSBinary",
 			path: "XCFrameworks/SIDSDKDynamic-CS.zip"
 		),
 
-
 		// Обёртка над статической библиотекой, куда «прокидываем» bundle. Без Кликстрима
 		.target(
 			name: "SIDSDKStaticWrapper_CS",
 			dependencies: [
-				"SIDSDKStaticBinary-CS"
+				.target(name: "SIDSDKStaticBinary-CS")
 			],
 			exclude: ["SIDSDKResourcesBundle.bundle/Info.plist"],
 			resources: [
