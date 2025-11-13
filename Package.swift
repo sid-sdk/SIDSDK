bc1169fe8b99b3a00755f0121e88f182b06ed3fe
Олюшкин Максим Константинович <MKoOlyushkin@sberbank.ru>
2025-11-13 12:35:10 +0300

    Релизная версия SIDSDK 1.9.0

--- Package.swift
+++ Package.swift
@@ -15,14 +15,6 @@ let package = Package(
 		.library(
 			name: "SIDSDKStatic",
 			targets: ["SIDSDKStaticWrapper"]
-		),
-		.library(
-			name: "SIDSDKDynamic-CS",
-			targets: ["SIDSDKDynamic-CSBinary"]
-		),
-		.library(
-			name: "SIDSDKStatic-CS",
-			targets: ["SIDSDKStaticWrapper_CS"]
 		)
 	],
 	dependencies: [
@@ -45,47 +37,22 @@ let package = Package(
 			path: "XCFrameworks/SIDSDKDynamic.zip"
 		),
 
-
 		// Обёртка над статической библиотекой, куда «прокидываем» bundle
 		.target(
 			name: "SIDSDKStaticWrapper",
 			dependencies: [
-				"SIDSDKStaticBinary",
+				.target(name: "SIDSDKStaticBinary"),
 				.product(name: "Clickstream", package: "iOS-SDK-Package")
-			],
-			exclude: ["SIDSDKResourcesBundle.bundle/Info.plist"], // исключаем Info.plist из bundle
-			resources: [
-				.process("SIDSDKResourcesBundle.bundle")
-			]
-		),		// Бинарная цель для статической библиотеки
-		.binaryTarget(
-			name: "SIDSDKStaticBinary",
-			path: "./XCFrameworks/SIDSDKStatic.zip"
-		),
-
-
-		// Бинарная цель для динамической библиотеки без Кликстрима
-		.binaryTarget(
-			name: "SIDSDKDynamic-CSBinary",
-			path: "XCFrameworks/SIDSDKDynamic-CS.zip"
-		),
-
-
-		// Обёртка над статической библиотекой, куда «прокидываем» bundle. Без Кликстрима
-		.target(
-			name: "SIDSDKStaticWrapper_CS",
-			dependencies: [
-				"SIDSDKStaticBinary-CS"
 			],
 			exclude: ["SIDSDKResourcesBundle.bundle/Info.plist"],
 			resources: [
 				.process("SIDSDKResourcesBundle.bundle")
 			]
 		),
-		// Бинарная цель для статической библиотеки без Кликстрима
+		// Бинарная цель для статической библиотеки
 		.binaryTarget(
-			name: "SIDSDKStaticBinary-CS",
-			path: "./XCFrameworks/SIDSDKStatic-CS.zip"
+			name: "SIDSDKStaticBinary",
+			path: "./XCFrameworks/SIDSDKStatic.zip"
 		)
 	]
 )
